f5962f6fe16ca052d5ea97b95dec9a2024883cc9
PractGit <deepa_1405@yahoo.com>
2021-02-18 20:17:16 +0530

    Committing First Maven project

---
+++ src/main/java/TestChromeDriver.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestChromeDriver {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resources/chromedriver.exe");
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		Thread.sleep(4000);
+	
+	}
+
+}
